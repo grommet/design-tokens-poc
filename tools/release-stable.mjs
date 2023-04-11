3820b6bedf4c6b63e8ef3376a7f85430ec35f595
Taylor Seamans <taylor.seamans@yahoo.com>
2023-04-11 09:12:30 -0700

    Add release-stable

---
+++ tools/release-stable.mjs
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+import del from 'del';
+import fs from 'fs-extra';
+import path from 'path';
+import { simpleGit as git } from 'simple-git';
+
+const repoURL = `https://${process.env.GH_TOKEN}@github.com/grommet/hpe-design-tokens.git`;
+const localFolder = path.resolve('.tmp/hpe-design-tokens');
+const localDist = path.resolve('dist');
+
+if (process.env.CI) {
+  del(localFolder).then(() => {
+    git()
+      .clone(repoURL, localFolder)
+      .then(() => git(localFolder).checkout('stable'))
+      .then(() => del([`${localFolder}/**/*`]))
+      .then(() => fs.copy(localDist, localFolder))
+      .then(() => git(localFolder).add(['--all', '.']))
+      .then(() => git(localFolder).commit('stable updated'))
+      .then(() => git(localFolder).push('origin', 'stable'))
+      .catch(err => console.error('failed: ', err));
+  });
+} else {
+  console.warn(
+    'Skipping release. Release:stable task should be executed by CI only.',
+  );
+}
