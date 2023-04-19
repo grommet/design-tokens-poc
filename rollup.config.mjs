44211d88aced3f2cd978c7e8d44f6fa8142af72d
Eric Soderberg <eric.soderberg@hpe.com>
2023-04-18 17:46:01 -0700

    Remove commented out code

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -25,21 +25,3 @@ export default {
     }),
   ],
 };
-
-// const config = [
-//   {
-//     input: ["index.ts"],
-//     output: {
-//       dir: "dist",
-//       format: "es",
-//     },
-//     plugins: [
-//       copy({
-//         targets: [
-//           { src: ['tokens.json', 'tokens.css'], dest: 'dist' },
-//         ]
-//       })
-//     ]
-//   },
-// ];
-// export default config;
