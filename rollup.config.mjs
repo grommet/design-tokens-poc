f85c754aa1b2783e6540366e72496ecd23e88189
Eric Soderberg <eric.soderberg@hpe.com>
2023-04-25 12:06:26 -0700

    Refactor build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,29 +1,45 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
 import pkg from './package.json' assert { type: 'json' };
 
-export default {
-  input: './index.ts',
-  output: [
-    {
-      file: './dist/index.js',
-      format: 'cjs',
-    },
-    {
-      file: './dist/index.mjs',
-      format: 'es',
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [
-    typescript(),
-    copy({
-      targets: [
-        { src: ['package.json', 'LICENSE'], dest: 'dist' },
-      ],
-    }),
-  ],
-};
+export default [
+  {
+    input: './index.ts',
+    output: [
+      {
+        dir: './dist/',
+        format: 'es',
+        preserveModules: true,
+      },
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      typescript(),
+      copy({
+        targets: [{ src: ['package.json', 'LICENSE'], dest: 'dist' }],
+      }),
+    ],
+  },
+  {
+    input: './index.ts',
+    output: [
+      {
+        dir: './dist/cjs/',
+        format: 'cjs',
+        preserveModules: true,
+      },
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.cjs.json',
+      }),
+    ],
+  },
+];
